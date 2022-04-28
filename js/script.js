f64626c495210cbd18d113d3ea1173f153981bc5
Zhenkaaf <36242555+Zhenkaaf@users.noreply.github.com>
2022-04-28 20:45:09 +0300

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        dots: true,
+        autoplay: true,
+    });
+});
+
+const iconMenu = document.querySelector('.menu__icon');
+const menuBody = document.querySelector('.menu__body');
+const page = document.querySelector('.page');
+const pageTitle = document.querySelector('.page__title');
+iconMenu.addEventListener('click', function(event) {
+    document.body.classList.toggle('_lock');
+    iconMenu.classList.toggle('_active');
+    menuBody.classList.toggle('_active');
+    page.classList.toggle('_active');
+    pageTitle.classList.toggle('_active');
+
+});
